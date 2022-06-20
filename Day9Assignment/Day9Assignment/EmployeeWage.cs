dad91270c4c17713a24c6df50f2386a4e58c2875
Balla <ballaphanindra@outlook.com>
2022-06-20 21:49:22 +0530

    updating UC1

---
+++ Day9Assignment/Day9Assignment/EmployeeWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Assignment
+{
+    internal class EmployeeWage
+    {
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation  Program on Master Branch");
+        }
+
+        public static void GetAttendance()
+        {
+            Random random = new Random();
+            int number = random.Next(0, 2);
+            CheckAttendance(number);
+        }
+
+        public static void CheckAttendance(int number)
+        {
+            if (number == 0)
+            {
+                Console.WriteLine("Employee is absent");
+            }
+            else
+            {
+                Console.WriteLine("Employee is present");
+            }
+        }
+    }
+}
