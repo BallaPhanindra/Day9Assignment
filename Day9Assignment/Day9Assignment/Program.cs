89b04823deecd4171c7a3d9bc94f2be202040dfb
Balla <ballaphanindra@outlook.com>
2022-06-20 21:31:18 +0530

    initial commit

---
+++ Day9Assignment/Day9Assignment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Day9Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RefactorEmpWage employeeWage = new RefactorEmpWage();
+
+            employeeWage.GetEmployeeWage("HCL", 20, 100, 20);
+        }
+    }
+}
